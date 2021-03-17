a75d272866d57bae4630ccb047323114af949ec0
@jeffersoncardoso <jeffersoncardoso100@gmail.com>
2021-03-17 01:17:22 -0300

    Inclusão arquivos atividade prévia

---
+++ demo-acme-ap/src/main/java/com/jeffersonsousa/demoacmeap/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.jeffersonsousa.demoacmeap.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeffersonsousa.demoacmeap.domain.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	public Optional<Cliente> findByCpf(String cpf);
+}
---
+++ demo-acme-ap/src/main/java/com/jeffersonsousa/demoacmeap/repository/FaturaRepository.java
@@ -0,0 +1,14 @@
+package com.jeffersonsousa.demoacmeap.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeffersonsousa.demoacmeap.domain.Fatura;
+import com.jeffersonsousa.demoacmeap.domain.Instalacao;
+
+public interface FaturaRepository extends JpaRepository<Fatura, Long> {
+	public Optional<Fatura> findByCodigo(String codigo);
+	public List<Fatura> findByInstalacao(Instalacao instalacao);
+}
